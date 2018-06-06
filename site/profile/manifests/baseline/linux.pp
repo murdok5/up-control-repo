6789528edce572f16b75e5787e2d99dcbba25a45
Kevin Hardy <hardyk@up.edu>
2018-06-06 15:16:58 -0700

    Initial Linux Baseline

--- site/profile/manifests/baseline/linux.pp
+++ site/profile/manifests/baseline/linux.pp
@@ -1,3 +1,39 @@
 class profile::baseline::linux {
 
+  # RHN vs. Spacewalk
+  # Timezone
+  class { 'timezone':
+      timezone => 'America/Los_Angeles',
+  }
+  # NTP
+  class { 'ntp':
+    servers => [ 'tick.campus.up.edu', 'tock.campus.up.edu' ],
+  }
+
+  # Shorewall - 22 Tyson - Nagios
+
+  # Sudo
+
+  class { 'sudo':
+    purge               => false,
+    config_file_replace => false,
+  }
+  sudo::conf { 'LinuxAdmin':
+    content => "%CAMPUS\\\\LinuxAdmin ALL=(ALL) NOPASSWD: ALL",
+  }
+  sudo::conf { 'sudo':
+    content => "%sudo ALL=(ALL) NOPASSWD: ALL",
+  }
+  sudo::conf { 'wheel':
+    content => "%wheel ALL=(ALL) NOPASSWD: ALL",
+  }
+
+  # Nagios
+  # nagios user, dev or prd group, password,
+
+  # SE Linux Enforce
+  class { selinux:
+    mode => 'enforcing',
+    type => 'targeted',
+  }
 }
