9b93fa65161f9608f981b542e4ff66ef52600ef1
Amy Norton <norton@up.edu>
2018-06-06 12:48:53 -0700

    created apps sub directory

--- site/profile/manifests/tomcat.pp
+++
@@ -1,5 +0,0 @@
-class profile::tomcat {
-
-
-
-}
--- site/role/manifests/tomcat_role.pp
+++ site/role/manifests/tomcat_role.pp
@@ -1,7 +1,7 @@
 class role::tomcat_role {
 
     include profile::base
-	include profile::tomcat
-	include profile::java
+	include profile::apps::tomcat
+	include profile::apps::java
 	
 }
