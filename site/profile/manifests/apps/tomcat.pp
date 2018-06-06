c970a1694f6ad5da5fd728c5be4f0bf7e8031b04
Amy Norton <norton@up.edu>
2018-06-06 14:06:05 -0700

    Initial tomcat setup

--- site/profile/manifests/apps/tomcat.pp
+++ site/profile/manifests/apps/tomcat.pp
@@ -1,5 +1,9 @@
+# here is a comment
 class profile::apps::tomcat {
-
-
-
+  tomcat::install { '/opt/tomcat':
+    source_url => 'https://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.31/bin/apache-tomcat-8.5.31.tar.gz',
+  }
+  tomcat::instance { 'default':
+    catalina_home => '/opt/tomcat',
+  }
 }
